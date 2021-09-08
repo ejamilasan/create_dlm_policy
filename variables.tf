aaf9cd9720340c30a0837ee353b52eb3268832eb
Edwin <59033523+ejamilasan@users.noreply.github.com>
2021-09-08 09:00:13 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+    description = "aws region"
+}
+
+variable "aws_account_alias" {
+    description = "aws account name"
+}
+
+variable "interval" {
+    description = "time interval"
+}
+
+variable "target_tags" {
+    description = "targeted tags"
+}
+
+variable "interval_unit" {
+    description = "interval unit"
+}
